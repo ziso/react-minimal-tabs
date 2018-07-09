2b5e8907f3320438d860bcc6ac2c656fe91235fb
ziso <omri.zisovitch@oath.com>
2018-07-09 15:58:29 +0300

    trying to use github-pages for demo

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
 });
 module.exports = {
 	entry: path.join(__dirname, "examples/src/index.js"),
+	output: {
+		path: path.join(__dirname, "examples/build"),
+		filename: "bundle.js"
+	},
 	module: {
 		rules: [
 			{
